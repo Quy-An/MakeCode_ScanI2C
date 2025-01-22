62fc0c524ebba338b834a32101436fd8b7a63d4c
Quy-An <quyan18b@gmail.com>
2025-01-22 14:54:47 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace ScanI2C{
     export function i2cCurrentAdd(){
         for (let i = 64; i < 69; i++) {
             pins.i2cWriteNumber(i, 0, NumberFormat.UInt8LE, false)
-            let data = pins.i2cReadNumber(i, NumberFormat.UInt8LE, false)
+            let data = pins.i2cReadNumber(i, NumberFormat.UInt8LE, true)
             if (data != 0x00) {
                 basic.showNumber(i)
                 basic.pause(500)
