e798f88f1f56488ccab5755ce00123d328e9e61f
Quy-An <quyan18b@gmail.com>
2025-01-22 15:58:48 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ namespace ScanI2C{
     //% blockId=ScanI2C
     //% block="i2cCurrentAdd"
     export function i2cCurrentAdd(){
+        let data = 0
         for (let i = 0; i < 128; i++) {
             pins.i2cWriteNumber(i, 0, NumberFormat.UInt8LE, false)
             let data = pins.i2cReadNumber(i, NumberFormat.UInt8LE, false)
