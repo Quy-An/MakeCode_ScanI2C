242dd6fde2d78aab2b2ff9c81a840259d144bcdf
Quy-An <quyan18b@gmail.com>
2025-01-22 14:05:02 +0700

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+namespace ScanI2C{
+    export function i2cCurrentAdd(){
+        for (let i = 64; i < 69; i++) {
+            pins.i2cWriteNumber(i, 0, NumberFormat.UInt8LE, false)
+            let data = pins.i2cReadNumber(i, NumberFormat.UInt8LE)
+            if (data != 0xFF) {
+                basic.showNumber(i)
+                basic.pause(500)
+            }
+        }
+    }
+}
