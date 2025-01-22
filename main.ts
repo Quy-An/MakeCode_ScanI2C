234e6b397687425e87f60b55ba358f2581b988f1
Quy-An <quyan18b@gmail.com>
2025-01-22 14:24:46 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ namespace ScanI2C{
     //% blockId=ScanI2C
     //% block="i2cCurrentAdd"
     export function i2cCurrentAdd(){
-        for (let i = 64; i < 69; i++) {
+        for (let i = 0; i < 127; i++) {
             pins.i2cWriteNumber(i, 0, NumberFormat.UInt8LE, false)
             let data = pins.i2cReadNumber(i, NumberFormat.UInt8LE)
             if (data != 0xFF) {
